34b1a013ba9f94a95fa6e4669221053a6bec218f
kevin66607 <rebonatokevin@gmail.com>
2025-06-30 07:21:18 +0200

    Add files via upload

---
+++ credenziali.js
@@ -0,0 +1,5 @@
+    const utenti=[
+        {"username":"admin","password":"123456","reparto":"BB"},
+        {"username":"a","password":"b","reparto":"BF"},
+        {"username":"kevin","password":"kevin1","reparto":"ALL"}
+    ];
